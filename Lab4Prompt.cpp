958b45940961a10c5267b1d2eb9878632cc66f34
Ян Красний <krasnyiya@fit.knu.ua>
2023-03-24 21:48:58 +0100

    Finished project, created second file with predefined limits

---
+++ Lab4Prompt.cpp
@@ -0,0 +1,52 @@
+//#include <iostream>
+//#include <cmath>
+//#include <conio.h>
+//#include <iomanip>
+//using namespace std;
+//
+//double funcCalc(double x)
+//{
+//    return pow(sin(x / 2), 3);
+//}
+//
+//int main()
+//{
+//    double a, b, dx, Integrl;
+//    int n;
+//
+//    char restart = 'r';
+//    while (restart == 'R' or restart == 'r')
+//    {
+//        cout << "Enter a: ";
+//        cin >> a;
+//        cout << "b: ";
+//        cin >> b;
+//        cout << "n: ";
+//        cin >> n;
+//        if (cin.fail() or n < 1)
+//        {
+//            cout << "Incorrect value! Should be a number that is greater than or equal to 1, but not greater than 9223372036854775807. Exiting the program";
+//            exit(1);
+//        }
+//
+//        dx = (b - a) / n;
+//
+//        Integrl = 0;
+//
+//        for (int i = 0; i < n; i++) 
+//        {
+//            double x = a + i * dx;
+//
+//            double f = funcCalc(x);
+//
+//            Integrl += f * dx;
+//        }
+//
+//        cout << "The integral is: " << fixed << setprecision(4) << Integrl << endl;
+//
+//        cout << "Press R to restart, any other key to exit" << endl;
+//        restart = _getch();
+//    }
+//
+//    return 0;
+//}
---
+++ Lab4WithValues.cpp
@@ -0,0 +1,50 @@
+#include <iostream>
+#include <cmath>
+#include <conio.h>
+#include <iomanip>
+using namespace std;
+
+double funcCalc(double x)
+{
+    return pow(sin(x / 2), 3);
+}
+
+int main()
+{
+    double dx, Integrl;
+    double a = 0;
+    double b = atan(1)*2;
+    int n;
+
+    char restart = 'r';
+    while (restart == 'R' or restart == 'r')
+    {
+        cout << "Enter the amount of sectors n: ";
+        cin >> n;
+        if (cin.fail() or n < 1)
+        {
+            cout << "Incorrect value! Should be a number that is greater than or equal to 1, but not greater than 9223372036854775807. Exiting the program";
+            exit(1);
+        }
+
+        dx = (b - a) / n;
+
+        Integrl = 0;
+
+        for (int i = 0; i < n; i++)
+        {
+            double x = a + i * dx;
+
+            double f = funcCalc(x);
+
+            Integrl += f * dx;
+        }
+
+        cout << "The integral is: " << fixed << setprecision(4) << Integrl << endl;
+
+        cout << "Press R to restart, any other key to exit" << endl;
+        restart = _getch();
+    }
+
+    return 0;
+}
